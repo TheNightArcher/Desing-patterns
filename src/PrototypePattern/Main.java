f2aeb8911948b4fb7f72adaa484e06a72f30fc1b
Galin Manov <thenightarchervclaymore@gmail.com>
2022-09-14 07:19:51 +0300

    Add example for Prototype pattern

---
+++ src/PrototypePattern/Main.java
@@ -0,0 +1,16 @@
+package PrototypePattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setBrand("Porsche");
+        car.setModel("911 Turbo");
+        car.setTopSpeed(320);
+
+        Car clone = car.clone();
+
+        System.out.printf("%s / %s / %d km", clone.getBrand(), clone.getModel(), clone.getTopSpeed());
+
+        // Result: Porsche / 911 Turbo / 320 km
+    }
+}
